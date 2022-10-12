d9f443ef322f3fd46dc41240677956cdcbce527d
MrSchmoke <martin.staats@gmail.com>
2022-10-12 13:47:41 +0200

    uninit favnum

--- Challenge01/main.cpp
+++ Challenge01/main.cpp
@@ -2,7 +2,7 @@
 
 int main() 
 {
-    int favnum = 0;
+    int favnum;
     std::cout << "Enter your favorite number between 1 and 100: " << std::endl;
     std::cin >> favnum;
     std::cout << "Amazing!! That's my favorite number too!" << std::endl;
